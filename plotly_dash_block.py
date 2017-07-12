289ed05f9bb258836de864816e86824825b97c99
Tom Young <tyoung@n.io>
2017-07-12 13:59:25 -0600

    spawn server in seperate thread

--- plotly_dash_block.py
+++ plotly_dash_block.py
@@ -1,5 +1,6 @@
 from nio.block.base import Block
 from nio.properties import VersionProperty
+from nio.util.threading.spawn import spawn
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -10,16 +11,30 @@ class PlotlyDash(Block):
     version = VersionProperty('0.1.0')
 
     def __init__(self):
+        self._main_thread = None
         self.app = dash.Dash()
         super().__init__()
 
     def start(self):
-        self.app.layout = html.Div()
-        self.app.run_server(debug=False)
+        self._main_thread = spawn(self._server)
+        self.logger.debug('server started on localhost:8050')
         super().start()
+
+    def stop(self):
+        try:
+            self._main_thread.join(1)
+            self.logger.debug('server stopped')
+        except:
+            self.logger.warning('main thread exited before join()')
+        super().stop()
 
     def process_signals(self, signals):
         graphs = []
         for signal in signals:
-            graphs.append(dcc.Graph(id=signal.title, figure={'data': [signal.data], 'layout': {'title': signal.title}}))
+            graphs.append(dcc.Graph(id=signal.title, figure={'data': signal.data, 'layout': {'title': signal.title}}))
         self.app.layout = html.Div(children=graphs)
+        self.logger.debug('displaying {} graphs '.format(len(graphs)))
+
+    def _server(self):
+        self.app.layout = html.Div()
+        self.app.run_server(debug=False)
