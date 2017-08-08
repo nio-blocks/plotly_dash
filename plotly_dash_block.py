885196da77b65f2b0d02c61faf4ed997dfb4b900
cowleyk <cowley.kevin.l@gmail.com>
2017-08-08 10:28:29 -0600

    Handle buffering inside block

--- plotly_dash_block.py
+++ plotly_dash_block.py
@@ -1,18 +1,35 @@
-from nio.block.base import Block
-from nio.properties import VersionProperty
-from nio.util.threading.spawn import spawn
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
+from nio.block.base import Block
+from nio.properties import VersionProperty, FloatProperty, Property, \
+    StringProperty, IntProperty
+from nio.util.threading.spawn import spawn
 
 
 class PlotlyDash(Block):
 
     version = VersionProperty('0.1.0')
+    title = StringProperty(
+        title="Title", default="Plotly Title", allow_none=False)
+    x_axis = Property(
+        title="X axis value",
+        default="{{ datetime.datetime.utcnow() }}",
+        allow_none=False
+    )
+    y_axis = FloatProperty(
+        title="Y axis value", default="{{ $y_data }}", allow_none=False)
+    num_data_points = IntProperty(
+        title="How many points to display", default=20, allow_none=True)
 
     def __init__(self):
         self._main_thread = None
         self.app = dash.Dash()
+        self.data = {
+            "x": [],
+            "y": []
+        }
         super().__init__()
 
     def start(self):
@@ -31,8 +48,16 @@ class PlotlyDash(Block):
     def process_signals(self, signals):
         graphs = []
         for signal in signals:
-            figure = {'data': signal.data, 'layout': {'title': signal.title}}
-            graphs.append(dcc.Graph(id=signal.title, figure=figure))
+            if len(self.data["x"]) < self.num_data_points():
+                self.data["x"].append(self.x_axis(signal))
+                self.data["y"].append(self.y_axis(signal))
+            else:
+                self.data["x"].append(self.x_axis(signal))
+                self.data["y"].append(self.y_axis(signal))
+                self.data["x"] = self.data["x"][1:]
+                self.data["y"] = self.data["y"][1:]
+            figure = {'data': [self.data], 'layout': {'title': self.title()}}
+            graphs.append(dcc.Graph(id=self.title(), figure=figure))
         self.app.layout = html.Div(children=graphs)
         self.logger.debug('displaying {} graphs '.format(len(graphs)))
 
