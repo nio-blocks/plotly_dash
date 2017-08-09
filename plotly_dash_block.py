0dfc05c55821b8a74da5af967bb775021709a651
cowleyk <cowley.kevin.l@gmail.com>
2017-08-09 14:54:46 -0600

    One graph, multiple series

--- plotly_dash_block.py
+++ plotly_dash_block.py
@@ -5,34 +5,43 @@ from dash.dependencies import Input, Output, Event
 
 from nio.block.base import Block
 from nio.properties import VersionProperty, FloatProperty, Property, \
-    StringProperty, IntProperty
+    StringProperty, IntProperty, PropertyHolder, ListProperty
 from nio.util.threading.spawn import spawn
+
+
+class Series(PropertyHolder):
+    y_axis = FloatProperty(
+        title='Dependent Variable', default='{{ $y_data }}', allow_none=False)
+    name = StringProperty(title='Series Name', default='', allow_none=False)
+    # kwargs = Property(title='Series Configurables', default='{{ {} }}')
+
+
+# class Graphs(PropertyHolder):
+#     id = Property(title='Graph Title', default='')
+#     series = ListProperty(Series, title='Series', default=[])
 
 
 class PlotlyDash(Block):
 
-
     version = VersionProperty('0.1.0')
-    title = StringProperty(
-        title="Title", default="Plotly Title", allow_none=False)
+    graph_series = ListProperty(Series, title='Data Series', default=[])
     x_axis = Property(
-        title="X axis value",
-        default="{{ datetime.datetime.utcnow() }}",
+        title='Independent Variable',
+        default='{{ datetime.datetime.utcnow() }}',
         allow_none=False
     )
-    y_axis = FloatProperty(
-        title="Y axis value", default="{{ $y_data }}", allow_none=False)
+    title = StringProperty(
+        title='Title', default='Plotly Title', allow_none=False)
     num_data_points = IntProperty(
-        title="How many points to display", default=20, allow_none=True)
+        title='How many points to display', default=20, allow_none=True)
 
+    # Does __init__ run when block is put in designer or on service start?
     def __init__(self):
         self._main_thread = None
         self.app = dash.Dash()
         self.app.config.supress_callback_exceptions=True
-        self.data = {
-            "x": [],
-            "y": []
-        }
+        self.data_dict = {}
+        self.data = []
         super().__init__()
 
     def start(self):
@@ -40,19 +49,20 @@ class PlotlyDash(Block):
         self.logger.debug('server started on localhost:8050')
         super().start()
 
-        figure = {'data': [self.data], 'layout': {'title': self.title()}}
-        self.app.layout = html.Div(
-            [dcc.Graph(id=self.title(), figure=figure),
-            dcc.Interval(
-                id='interval-component',
-                interval=1 * 1000  # in milliseconds
-            )]
-        )
+        self.data_dict = {s.name(): {'x': [], 'y': [], 'name': s.name()} for s in self.graph_series()}
+        self.data = self.data_dict_to_data_list(self.data_dict)
+        figure = {'data': self.data, 'layout': {'title': self.title()}}
+        app_layout = [
+            dcc.Graph(id=self.title(), figure=figure),
+            dcc.Interval(id='interval-component', interval=1 * 1000)
+        ]
+
+        self.app.layout = html.Div(app_layout)
+
         @self.app.callback(Output(self.title(), 'figure'),
                            events=[Event('interval-component', 'interval')])
         def update_graph_live():
-            return {'data': [self.data], 'layout': {'title': self.title()}}
-
+            return {'data': self.data, 'layout': {'title': self.title()}}
 
     def stop(self):
         try:
@@ -63,17 +73,23 @@ class PlotlyDash(Block):
         super().stop()
 
     def process_signals(self, signals):
-        graphs = []
+        # process_signals just needs to update self.data list
+        # append new signal data to the proper dict key
         for signal in signals:
-            if len(self.data["x"]) < self.num_data_points():
-                self.data["x"].append(self.x_axis(signal))
-                self.data["y"].append(self.y_axis(signal))
-            else:
-                self.data["x"].append(self.x_axis(signal))
-                self.data["y"].append(self.y_axis(signal))
-                self.data["x"] = self.data["x"][1:]
-                self.data["y"] = self.data["y"][1:]
-        self.logger.debug('displaying {} graphs '.format(len(graphs)))
+            for series in self.graph_series():
+                if len(self.data_dict[series.name()]['y']) < self.num_data_points():
+                    self.data_dict[series.name()]['x'].append(self.x_axis(signal))
+                    self.data_dict[series.name()]['y'].append(series.y_axis(signal))
+                else:
+                    self.data_dict[series.name()]['x'].append(self.x_axis(signal))
+                    self.data_dict[series.name()]['y'].append(series.y_axis(signal))
+                    self.data_dict[series.name()]['x'] = self.data_dict[series.name()]['x'][1:]
+                    self.data_dict[series.name()]['y'] = self.data_dict[series.name()]['y'][1:]
+        self.data = self.data_dict_to_data_list(self.data_dict)
+
+    @staticmethod
+    def data_dict_to_data_list(dict):
+        return [v for d,v in dict.items()]
 
     def _server(self):
         self.app.layout = html.Div()
